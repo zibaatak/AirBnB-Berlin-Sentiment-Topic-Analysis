417d638af511149304e5f75c17568621119d049e
Ziba <zibaatak@gmail.com>
2023-09-03 12:04:58 +0300

    app.py

--- app.py
+++ app.py
@@ -4,14 +4,6 @@ import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import MinMaxScaler
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from IPython.display import Image, display, HTML
-
-# Download NLTK data (if not already downloaded)
-nltk.download('punkt')
-nltk.download('stopwords')
 
 # Load the preprocessed DataFrame
 def load_data():
